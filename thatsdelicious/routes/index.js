21ff6a5ce3106e4f0af90a98ac9db8dc8cb2cfb1
Jacob Bryant <jacobryas4@gmail.com>
2020-01-28 18:10:32 -0500

    stored user in database and made protected routes for AddStore

--- thatsdelicious/routes/index.js
+++ thatsdelicious/routes/index.js
@@ -2,12 +2,14 @@ const express = require('express');
 const router = express.Router();
 const storeController = require('../controllers/storeController')
 const userController = require('../controllers/userController')
+const authController = require('../controllers/authController')
 const { catchErrors } = require('../handlers/errorHandlers')
+
 
 // Do work here
 router.get('/', catchErrors(storeController.getStores))
 router.get('/stores', catchErrors(storeController.getStores))
-router.get('/add', storeController.addStore)
+router.get('/add', authController.isLoggedIn ,storeController.addStore)
 
 router.post('/add', 
     storeController.upload, 
@@ -27,7 +29,14 @@ router.get('/tags', catchErrors(storeController.getStoresByTag))
 router.get('/tags/:tag', catchErrors(storeController.getStoresByTag))
 
 router.get('/login', userController.loginForm)
+router.post('/login', authController.login)
 router.get('/register', userController.registerForm)
-router.post('/register', userController.validateRegister)
+router.post('/register', 
+    userController.validateRegister,
+    userController.register,
+    authController.login
+)
+
+router.get('/logout', authController.logout)
 
 module.exports = router;
