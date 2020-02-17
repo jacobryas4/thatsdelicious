181a446a0468a34d1748d1aefa9e34f386e375cd
Jacob Bryant <jacobryas4@gmail.com>
2020-02-17 16:54:46 -0500

    added pagination

--- thatsdelicious/controllers/storeController.js
+++ thatsdelicious/controllers/storeController.js
@@ -56,11 +56,28 @@ exports.createStore =  async (req, res) => {
 }
 
 exports.getStores = async (req,res) => {
+    const page = req.params.page || 1
+    const limit = 6
+    const skip = (page * limit) - limit
     // Query DB for list of all stores
-    const stores = await Store.find()
-    console.log(stores)
+    const storesPromise = Store
+        .find()
+        .skip(skip)
+        .limit(limit)
+        .sort({ created: 'desc' })
 
-    res.render('stores', { title: 'Stores', stores })
+    const countPromise = Store.count()
+
+    const [stores, count] = await Promise.all([storesPromise, countPromise])
+    const pages = Math.ceil(count / limit)
+
+    if(!stores.length && skip) {
+        req.flash('info', `You asked for page ${page} but that doesn't exist. So now you're on page ${pages}`)
+        res.redirect(`/stores/page/${pages}`)
+        return
+    }
+
+    res.render('stores', { title: 'Stores', stores, page, pages, count })
 }
 
 const confirmOwner = (store, user) => {
